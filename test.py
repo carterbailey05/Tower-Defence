1fb1def24d80af8778b1d2fb6395ec6e36444154
John Holmes <holmesjw@gmail.com>
2018-06-11 13:09:42 -0400

    merged

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 #test
-<<<<<<< HEAD
-for x in range (2, 12):
-    print (x)
-print (x)
-=======
+
+
 for i in range (2, 12):
     print (i)
+print (i)
     #Let's not forget to comment.
 
     
->>>>>>> 39bbb3db3f478cb3fbdf31de19120fe32734c380
